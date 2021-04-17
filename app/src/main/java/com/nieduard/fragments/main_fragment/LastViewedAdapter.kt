79489906f99ea19b87beb2d7ceb585a699bd61f7
EdwardNee <nieduard_01@mail.ru>
2021-04-17 17:45:25 +0300

    LastViewedAdapter.kt completed

--- app/src/main/java/com/nieduard/fragments/main_fragment/LastViewedAdapter.kt
+++ app/src/main/java/com/nieduard/fragments/main_fragment/LastViewedAdapter.kt
@@ -1,12 +1,45 @@
 package com.nieduard.fragments.main_fragment
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.nieduard.data.LastViewedData
+import com.nieduard.prj.R
 
-class LastViewedAdapter(private val recentViewedList : List<LastViewedData>) : RecyclerView.Adapter<> {
+/**
+ * Adapter for recently viewed items in main fragment.
+ */
+class LastViewedAdapter(private val recentViewedList: List<LastViewedData>) :
+    RecyclerView.Adapter<LastViewedViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LastViewedViewHolder {
+        return LastViewedViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.view_holder_recent, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: LastViewedViewHolder, position: Int) {
+        val currentItem = recentViewedList[position]
+        holder.onBind(currentItem)
+    }
+
+    override fun getItemCount() = recentViewedList.size
 }
 
-class LastViewedViewHolder(private val itemView : View) : RecyclerView.ViewHolder(itemView){
+/**
+ * RecyclerView to hold the data.
+ */
+class LastViewedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val imageView: ImageView = itemView.findViewById(R.id.r_image)
+    private val price: TextView = itemView.findViewById(R.id.r_price)
+    private val name: TextView = itemView.findViewById(R.id.r_name)
 
+    fun onBind(item: LastViewedData) {
+        val context = itemView.context
+        imageView.setImageResource(item.imageResource)
+        price.text = context.getString(R.string.string_price, item.price)
+        name.text = context.getString(R.string.string_name, item.name)
+    }
 }
